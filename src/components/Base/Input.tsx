1c282a7b707f16e5c0eb5d0f1c04570d8cd0f0bf
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-02 17:15:48 -0300

    refactor: Remove remove state and input attributes

--- src/components/Base/Input.tsx
+++ src/components/Base/Input.tsx
@@ -1,24 +1,16 @@
-import { ChangeEvent, FC, InputHTMLAttributes, useState } from "react";
+import { FC, InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
 const Input: FC<InputProps> = ({ label, ...props }) => {
-  const [value, setValue] = useState("");
-
-  const onChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    setValue(ev.target.value);
-  };
-
   return (
     <div className="flex flex-row justify-between ">
-      <p className="font-medium text-xl text-default">{label}</p>
+      <p className="text-xl font-medium text-default">{label}</p>
       <input
         placeholder="--------"
-        className="focus:outline-none focus:border-b-2 focus:border-gradientFirst font-bold text-right w-1/2 text-transparent bg-clip-text bg-gradient-to-br from-gradientFirst to-gradientSecond placeholder:text-transparent placeholder:bg-clip-text placeholder:bg-gradient-to-br placeholder:from-gradientFirst placeholder:to-gradientSecond"
-        onChange={onChange}
-        value={value}
+        className="w-1/2 bg-gradient-to-br from-gradientFirst to-gradientSecond bg-clip-text text-right font-bold text-transparent placeholder:bg-gradient-to-br placeholder:from-gradientFirst placeholder:to-gradientSecond placeholder:bg-clip-text placeholder:text-transparent focus:border-b-2 focus:border-gradientFirst focus:outline-none"
         {...props}
       ></input>
     </div>
