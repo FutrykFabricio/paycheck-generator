12dd85b90bb88a6c4a5c128de9dedd60fe794b02
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-02 17:16:54 -0300

    feat: Add functionality with paycheck context

--- src/components/Absence.tsx
+++ src/components/Absence.tsx
@@ -1,45 +1,55 @@
-import { useState } from "react";
+import {
+  DEFAULT_PRESENTEEISM,
+  MAX_PRESENTEEISM,
+  MIN_PRESENTEEISM,
+} from "../constants";
+import usePaycheck from "../hooks/usePaycheck";
 import AbsenceInput from "./AbsenceInput";
 import Button from "./Base/Button";
 
 const Absence = () => {
-  const [presenteeism, setPresenteeism] = useState<number>(25);
-  const [quantity, setQuantity] = useState<number>(0);
+  const { get, set } = usePaycheck();
 
   const addAbsence = () => {
-    if (presenteeism > 0 && quantity < 5) {
-      setQuantity(quantity + 1);
-      setPresenteeism(presenteeism - 5);
+    if (
+      DEFAULT_PRESENTEEISM - get.deductions.absents.length * 5 >
+      MIN_PRESENTEEISM
+    ) {
+      set.deductions({
+        ...get.deductions,
+        absents: [...get.deductions.absents, { date: new Date() }],
+      });
     }
   };
 
   const removeAbsence = () => {
-    if (presenteeism < 25 && quantity > 0) {
-      setQuantity(quantity - 1);
-      setPresenteeism(presenteeism + 5);
+    if (
+      DEFAULT_PRESENTEEISM - get.deductions.absents.length * 5 <
+      MAX_PRESENTEEISM
+    ) {
+      set.deductions({
+        ...get.deductions,
+        absents: get.deductions.absents.slice(
+          0,
+          get.deductions.absents.length - 1
+        ),
+      });
     }
   };
 
   return (
     <div>
       <div className="flex w-full flex-row items-center justify-between">
-        <p className="text-xl font-medium text-default">
-          Faltas{" "}
-          <span className="text-xs font-light">
-            (Presentismo: {presenteeism}%)
-          </span>
-        </p>
+        <p className="text-xl font-medium text-default">Faltas</p>
         <div className="flex flex-row gap-5">
           <Button onClick={addAbsence}>+</Button>
           <Button onClick={removeAbsence}>-</Button>
         </div>
       </div>
       <div className="flex flex-col gap-2">
-        {Array(quantity)
-          .fill(null)
-          .map((_, key) => {
-            return <AbsenceInput key={key} />;
-          })}
+        {get.deductions.absents.map((_, key) => {
+          return <AbsenceInput index={key} key={key} />;
+        })}
       </div>
     </div>
   );
