f18cbd5e8cf3bbc5ee7e81d24a50df273c11a4d1
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-02 17:04:45 -0300

    feat: Add constants

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const PRODUCTION = 10;
+export const INSSJP = 3;
+export const SOCIAL_WORK = 3;
+export const SIPA = 11;
+export const MAX_PRESENTEEISM = 25;
+export const DEFAULT_PRESENTEEISM = 25;
+export const MIN_PRESENTEEISM = 0;
+export const HOURS = 176;
