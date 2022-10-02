8adf8f521f5d623505abcba34a15222e9efeb234
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-02 17:16:37 -0300

    feat: Add functionality with paycheck context

--- src/components/AbsenceInput.tsx
+++ src/components/AbsenceInput.tsx
@@ -1,13 +1,32 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
+import usePaycheck from "../hooks/usePaycheck";
 import Checkbox from "./Base/Checkbox";
 
-const AbsenceInput = () => {
+interface AbsenceInputProps {
+  index: number;
+}
+
+const AbsenceInput: FC<AbsenceInputProps> = ({ index }) => {
+  const { get, set } = usePaycheck();
   const [justified, setJustified] = useState<boolean>(false);
-  const [date, setDate] = useState<Date | null>(null);
+  const [date, setDate] = useState<Date>(new Date());
 
   const onDateChange = (ev: ChangeEvent<HTMLInputElement>) => {
     setDate(new Date(ev.target.value));
   };
+
+  useEffect(() => {
+    const absenceCopy = [...get.deductions.absents];
+    const absent = { ...absenceCopy[index] };
+
+    absent.date = date;
+    absent.justified = justified;
+
+    absenceCopy[index] = absent;
+
+    set.deductions({ ...get.deductions, absents: absenceCopy });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [justified, date]);
 
   return (
     <div className="flex flex-row justify-between">
