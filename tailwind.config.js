f550fcf7111e703c2cf467fd03a0c1a0d501102e
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-02 01:37:58 -0300

    feat: Extend colors and fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gradientFirst: "#FD746C",
+        gradientSecond: "#FF9068",
+        default: "#262626",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
