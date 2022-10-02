a0b5b61112d90a7eb8c1960e05ea7ed3657b03a2
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-02 01:39:40 -0300

    feat: Add result value component

---
+++ src/components/ResultValue.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+
+interface ResultValueProps {
+  label: string;
+  value: number;
+  percentage?: string;
+}
+
+const intl = new Intl.NumberFormat("es-ES", {
+  style: "currency",
+  currency: "ARS",
+});
+
+const ResultValue: FC<ResultValueProps> = ({ label, value, percentage }) => {
+  return (
+    <div className="flex flex-row justify-between text-default">
+      <p className="text-base font-bold">
+        {label}
+        {percentage && (
+          <span className="text-xs font-normal">({percentage}%)</span>
+        )}
+      </p>
+      <p className="text-lg font-thin">${intl.format(value)}</p>
+    </div>
+  );
+};
+
+export default ResultValue;
