b0ecedac3b477c83c02d5e6c8f65e0b0e00b7457
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-02 01:39:07 -0300

    feat: Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+const Header: FC = () => (
+  <header className="bg-gradient-to-br from-gradientFirst to-gradientSecond p-2 text-center text-xl font-bold text-white shadow-md md:text-left">
+    <p>Generador de Recibos de Sueldo</p>
+  </header>
+);
+export default Header;
