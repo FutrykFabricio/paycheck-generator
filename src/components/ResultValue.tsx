eea15f24bc47f7943d61e8de6599bf492bcb6533
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-14 15:19:46 -0300

    feat: Add tooltip

--- src/components/ResultValue.tsx
+++ src/components/ResultValue.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
+import Tooltip from "./Tooltip";
 
 interface ResultValueProps {
   label: string;
   value: number;
   percentage?: string;
+  tooltip?: string;
 }
 
 const intl = new Intl.NumberFormat("es-ES", {
@@ -11,15 +13,31 @@ const intl = new Intl.NumberFormat("es-ES", {
   currency: "ARS",
 });
 
-const ResultValue: FC<ResultValueProps> = ({ label, value, percentage }) => {
+const ResultValue: FC<ResultValueProps> = ({
+  label,
+  value,
+  percentage,
+  tooltip,
+}) => {
   return (
     <div className="flex flex-row justify-between text-default">
-      <p className="text-base font-bold">
-        {label}
-        {percentage && (
-          <span className="text-xs font-normal">({percentage}%)</span>
-        )}
-      </p>
+      {tooltip ? (
+        <Tooltip tooltipContent={tooltip}>
+          <p className="text-base font-bold">
+            {label}
+            {percentage && (
+              <span className="text-xs font-normal">({percentage}%)</span>
+            )}
+          </p>
+        </Tooltip>
+      ) : (
+        <p className="text-base font-bold">
+          {label}
+          {percentage && (
+            <span className="text-xs font-normal">({percentage}%)</span>
+          )}
+        </p>
+      )}
       <p className="text-lg font-thin">${intl.format(value)}</p>
     </div>
   );
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
 import {
   DEFAULT_PRESENTEEISM,
+  HOURS,
   INSSJP,
   PRODUCTION,
   SIPA,
@@ -11,6 +12,8 @@ import ResultValue from "./ResultValue";
 
 const Results = () => {
   const { get } = usePaycheck();
+  const hourValue = get.values.wage / HOURS;
+
   return (
     <div>
       <div className="text-md ml-4 flex flex-row items-center gap-2 text-[#6A6A6A]">
@@ -21,9 +24,18 @@ const Results = () => {
       </div>
       <div>
         <div className="my-4 border-b-2 border-[#D3D3D3]">
-          <ResultValue label="Básico" value={get.values.wage} />
+          <ResultValue
+            label="Básico"
+            value={get.values.wage}
+            tooltip={`${
+              get.paymentForm.wage
+            }$ - ${get.deductions.absence.toFixed(
+              2
+            )}$ + ${get.values.justified.toFixed(2)}$`}
+          />
           <ResultValue
             label="Produccion"
+            tooltip={`${PRODUCTION}% de ${get.values.wage.toFixed(2)}$`}
             percentage={`${PRODUCTION}`}
             value={get.values.production}
           />
@@ -33,26 +45,45 @@ const Results = () => {
             percentage={`${
               DEFAULT_PRESENTEEISM - get.deductions.absents.length * 5
             }`}
+            tooltip={`${
+              DEFAULT_PRESENTEEISM - get.deductions.absents.length * 5
+            }% de ${get.values.wage.toFixed(2)}$`}
           />
           <ResultValue
             label="Antigüedad"
             percentage={`${get.paymentForm.seniority}`}
             value={get.values.seniority}
+            tooltip={`${
+              get.paymentForm.seniority
+            }% de ${get.values.wage.toFixed(2)}$`}
           />
-          <ResultValue label="Feriados" value={get.values.holidays} />
+          <ResultValue
+            label="Feriados"
+            value={get.values.holidays}
+            tooltip={`${get.paymentForm.holidays} * ${hourValue.toFixed(
+              2
+            )}$ * 8`}
+          />
           <ResultValue
             label="Horas Extra"
             percentage="50"
             value={get.values.extraHalf}
+            tooltip={`${hourValue.toFixed(2)}$ * ${
+              get.paymentForm.extraHalf
+            } * 1.5`}
           />
           <ResultValue
             label="Horas Extra"
             percentage="100"
             value={get.values.extraFull}
+            tooltip={`${hourValue.toFixed(2)}$ * ${
+              get.paymentForm.extraFull
+            } * 2`}
           />
           <ResultValue
             label="Faltas Justificadas"
             value={get.values.justified}
+            tooltip={""}
           />
         </div>
         <div className="my-4 border-b-2 border-[#D3D3D3]">
@@ -60,18 +91,27 @@ const Results = () => {
             label="SIPA"
             percentage={`${SIPA}`}
             value={get.deductions.SIPA}
+            tooltip={`${SIPA}% de ${get.values.rawRemuneration.toFixed(2)}$`}
           />
           <ResultValue
             label="INSSJP"
             percentage={`${INSSJP}`}
             value={get.deductions.INSSJP}
+            tooltip={`${INSSJP}% de ${get.values.rawRemuneration.toFixed(2)}$`}
           />
           <ResultValue
             label="Obra Social"
             percentage={`${SOCIAL_WORK}`}
             value={get.deductions.socialWork}
+            tooltip={`${SOCIAL_WORK}% de ${get.values.rawRemuneration.toFixed(
+              2
+            )}$`}
           />
-          <ResultValue label="Faltas" value={get.deductions.absence} />
+          <ResultValue
+            label="Faltas"
+            value={get.deductions.absence}
+            tooltip={""}
+          />
         </div>
         <div className="my-4">
           <ResultValue
